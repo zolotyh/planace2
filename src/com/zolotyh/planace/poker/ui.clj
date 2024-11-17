f91ce2a582c86a7dc38f8976c73321bb6cbddfd4
aleksei.zolotykh <aazolotyh@gmail.com>
2024-11-17 14:47:45 +0300

    add pico.min.css

--- src/com/zolotyh/planace/poker/ui.clj
+++ src/com/zolotyh/planace/poker/ui.clj
@@ -5,16 +5,8 @@
    [com.zolotyh.planace.settings :as settings]
    [ring.util.response :as ring-response]))
 
-(defn css-path [path]
-  (if-some [last-modified (some-> (io/resource "public/css/main.css")
-                                  ring-response/resource-data
-                                  :last-modified
-                                  (.getTime))]
-    (str path "?t=" last-modified)
-    path))
-
-(defn js-path [path]
-  (if-some [last-modified (some-> (io/resource "public/js/main.js")
+(defn static [path]
+  (if-some [last-modified (some-> (io/resource (str "public" path))
                                   ring-response/resource-data
                                   :last-modified
                                   (.getTime))]
@@ -31,12 +23,12 @@
                      :description (str settings/app-name " Description")
                      :image "https://clojure.org/images/clojure-logo-120b.png"})
        (update :base/head (fn [head]
-                            (concat [[:link {:rel "stylesheet" :href (css-path "/css/mvp.css")}]
-                                     [:link {:rel "stylesheet" :href (css-path "/css/main.css")}]
-                                     [:script {:src (js-path "/js/main.js")}]
-                                     [:script {:src (js-path "/js/htmx.min.js")}]
-                                     [:script {:src (js-path "/js/alpine.min.js")}]
-                                     [:script {:src (js-path "/js/ws.js")}]
+                            (concat [[:link {:rel "stylesheet" :href (static "/css/pico.min.css")}]
+                                     [:link {:rel "stylesheet" :href (static "/css/main.css")}]
+                                     [:script {:src (static "/js/main.js")}]
+                                     [:script {:src (static "/js/htmx.min.js")}]
+                                     [:script {:src (static "/js/alpine.min.js")}]
+                                     [:script {:src (static "/js/ws.js")}]
                                      (when recaptcha
                                        [:script {:src "https://www.google.com/recaptcha/api.js"
                                                  :async "async" :defer "defer"}])]
