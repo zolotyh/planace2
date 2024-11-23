f499d60ab39bc418e48093eeb99d6ddb8f44d883
aleksei.zolotykh <aazolotyh@gmail.com>
2024-11-23 22:38:01 +0300

    update schema

--- src/com/zolotyh/planace/schema.clj
+++ src/com/zolotyh/planace/schema.clj
@@ -14,7 +14,29 @@
          [:xt/id       :msg/id]
          [:msg/user    :user/id]
          [:msg/text    :string]
-         [:msg/sent-at inst?]]})
+         [:msg/sent-at inst?]]
+
+   :room/id :uuid
+   :room [:map {:closed true}
+          [:xt/id :room/id]
+          [:room/title :string]
+          [:room/current-vote :vote/id]
+          [:room/owner :user/id]
+          [:room/default-vote-type :vote/vote-type]
+          [:room/members [:vector :user/id]]]
+
+   :vote/vote-type [:enum :fib :natural :t-shirts]
+   :vote/id :uuid
+   :vote [:map {:closed true}
+          [:xt/id :vote/id]
+          [:vote/room :room/id]
+          [:vote/open :boolean]
+          [:vote/type :vote/vote-type]
+          [:vote/title :string]
+          [:vote/result [:vector
+                         [:map {:closed true}
+                          [:vote {:optional true} int?]
+                          [:user :user/id]]]]]})
 
 (def module
   {:schema schema})
