b60fea39bfbb824238449fa8a38e5e04f095543e
aleksei.zolotykh <aazolotyh@gmail.com>
2024-11-24 03:25:24 +0300

    add random id generator

--- src/com/zolotyh/planace/poker/url_utils.clj
+++ src/com/zolotyh/planace/poker/url_utils.clj
@@ -1,6 +1,16 @@
 (ns com.zolotyh.planace.poker.url-utils
   (:require
+   [clojure.string]
    [reitit.core :as r]))
 
 (defn reverse-url [{:keys [reitit.core/router path-params]} item]
   (merge {:url (r/match-by-name router ::room-item (merge path-params item))} item))
+
+(defn generate-public-id []
+  (->>
+   (range 16)
+   (map (fn [_] (rand-int 9)))
+   (clojure.string/join "")))
+
+(defn beatify-public-id [id]
+  (re-seq #".{1,4}" id))
